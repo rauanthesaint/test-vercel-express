4d859cb1e1b36f84114eff4e8efe7d7ea65f9d14
rauantehsaint <rauanthesaint@gmail.com>
2025-01-26 12:45:08 +0500

    Testgin json v1.1

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,12 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hi",
+  });
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000"));
 
 export default app;
