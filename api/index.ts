cc51ba0c20e514c04efcb8b03a72fd752ffb2c48
rauantehsaint <rauanthesaint@gmail.com>
2025-01-26 12:46:13 +0500

    Testgin json v1.2

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,6 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-
-app.get("/", (req, res) => {
   res.json({
     message: "Hi",
   });
